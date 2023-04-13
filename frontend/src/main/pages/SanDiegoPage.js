d973197a9e41a50ef0123fd8a9c540dcd36f2db1
Jessica Ho <shin-yi@umail.ucsb.edu>
2023-04-13 12:50:30 -0700

    jh - add page for San Diego

---
+++ frontend/src/main/pages/SanDiegoPage.js
@@ -0,0 +1,15 @@
+import BasicLayout from "main/layouts/BasicLayout/BasicLayout";
+
+export default function SanDiegoPage() {
+  return (
+    <BasicLayout>
+      <div className="pt-2">
+        <h1>San Diego, CA</h1>
+        <p>
+         San Diego, California is a city on the Pacific coast of California known for its beautiful beaches, parks and warm climate.
+        </p>
+        <p>Added by Jessica H.</p>
+      </div>
+    </BasicLayout>
+  )
+}
