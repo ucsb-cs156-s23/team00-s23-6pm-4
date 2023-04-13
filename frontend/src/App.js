6a19647ff248febc3b8ecf8ae279045c38371c9a
Krish Agarwal <kshitij0407@gmail.com>
2023-04-12 18:18:50 -0700

    changed team name

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,7 +10,7 @@ import "bootstrap/dist/css/bootstrap.css";
 function App() {
 
   return (
-    <BrowserRouter basename="/STARTER-team00">
+    <BrowserRouter basename="/team00-s23-6pm-4">
       <Routes>
         <Route exact path="/" element={<HomePage />} />
         <Route exact path="/towns/AvilaBeach" element={<AvilaBeachPage />} />
