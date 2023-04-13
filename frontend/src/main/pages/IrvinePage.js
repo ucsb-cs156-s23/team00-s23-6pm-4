0de24591b087c6a1e8587263be71dc0e3107e6d4
Ashley Budman <ashleybudman@umail.ucsb.edu>
2023-04-13 14:55:50 -0700

    ab - add page for Irvine

--- frontend/src/main/pages/IrvinePage.js
+++ frontend/src/main/pages/IrvinePage.js
@@ -9,7 +9,11 @@ export default function IrvinePage() {
          <b>Irvine, California</b> is a town in Orange County, to
          the southeast of Los Angeles.
          Irvine is a true Southern California vacation where visitors
+<<<<<<< HEAD
          can enjoy miles of hiking, beaches, shopping,
+=======
+         can enjoy miles of hiking, golf, shopping,
+>>>>>>> 6597554 (ab - add page for Irvine)
          and international dining!
         </p>
         <p>Added by Ashley B.</p>
